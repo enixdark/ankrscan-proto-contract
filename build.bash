60e8e9217fdd96e40c742ef73be54aa77adc01a1
khasan <xozzslip@gmail.com>
2022-02-04 12:10:51 +0300

    update proto

--- build.bash
+++ build.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-protoc -I./proto -I./deps --go_out=generated/go --go-grpc_out=generated/go --grpc-gateway_out=generated/go --grpc-gateway_opt logtostderr=true --openapiv2_out generated/swagger proto/*
+protoc -I./proto -I./deps --go_out=generated/go/proto --go-grpc_out=generated/go/proto --grpc-gateway_out=generated/go/proto --grpc-gateway_opt logtostderr=true --openapiv2_out generated/swagger proto/*
