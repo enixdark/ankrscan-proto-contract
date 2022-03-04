a365044616dfcecf0b01cfa68ea697898ec7c078
khasan <xozzslip@gmail.com>
2022-03-04 14:47:25 +0300

    update proto

--- go/proto/token-indexer.pb.go
+++ go/proto/token-indexer.pb.go
@@ -225,6 +225,69 @@ func (x *TokenContractRequest) GetAddress() []byte {
 	return nil
 }
 
+type TokenBalanceRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	BlockchainName string `protobuf:"bytes,1,opt,name=blockchain_name,json=blockchainName,proto3" json:"blockchain_name,omitempty"`
+	Address        []byte `protobuf:"bytes,2,opt,name=address,proto3" json:"address,omitempty"`
+	BlockHeight    uint64 `protobuf:"varint,3,opt,name=block_height,json=blockHeight,proto3" json:"block_height,omitempty"`
+}
+
+func (x *TokenBalanceRequest) Reset() {
+	*x = TokenBalanceRequest{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_token_indexer_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *TokenBalanceRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*TokenBalanceRequest) ProtoMessage() {}
+
+func (x *TokenBalanceRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_token_indexer_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use TokenBalanceRequest.ProtoReflect.Descriptor instead.
+func (*TokenBalanceRequest) Descriptor() ([]byte, []int) {
+	return file_token_indexer_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *TokenBalanceRequest) GetBlockchainName() string {
+	if x != nil {
+		return x.BlockchainName
+	}
+	return ""
+}
+
+func (x *TokenBalanceRequest) GetAddress() []byte {
+	if x != nil {
+		return x.Address
+	}
+	return nil
+}
+
+func (x *TokenBalanceRequest) GetBlockHeight() uint64 {
+	if x != nil {
+		return x.BlockHeight
+	}
+	return 0
+}
+
 var File_token_indexer_proto protoreflect.FileDescriptor
 
 var file_token_indexer_proto_rawDesc = []byte{
@@ -257,9 +320,17 @@ var file_token_indexer_proto_rawDesc = []byte{
 	0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0e, 0x62, 0x6c, 0x6f, 0x63, 0x6b, 0x63, 0x68,
 	0x61, 0x69, 0x6e, 0x4e, 0x61, 0x6d, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x61, 0x64, 0x64, 0x72, 0x65,
 	0x73, 0x73, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x07, 0x61, 0x64, 0x64, 0x72, 0x65, 0x73,
-	0x73, 0x32, 0x0e, 0x0a, 0x0c, 0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x49, 0x6e, 0x64, 0x65, 0x78, 0x65,
-	0x72, 0x42, 0x0a, 0x5a, 0x08, 0x2e, 0x2f, 0x3b, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x06, 0x70,
-	0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x73, 0x22, 0x7b, 0x0a, 0x13, 0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x42, 0x61, 0x6c, 0x61, 0x6e, 0x63,
+	0x65, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x27, 0x0a, 0x0f, 0x62, 0x6c, 0x6f, 0x63,
+	0x6b, 0x63, 0x68, 0x61, 0x69, 0x6e, 0x5f, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28,
+	0x09, 0x52, 0x0e, 0x62, 0x6c, 0x6f, 0x63, 0x6b, 0x63, 0x68, 0x61, 0x69, 0x6e, 0x4e, 0x61, 0x6d,
+	0x65, 0x12, 0x18, 0x0a, 0x07, 0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x18, 0x02, 0x20, 0x01,
+	0x28, 0x0c, 0x52, 0x07, 0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x12, 0x21, 0x0a, 0x0c, 0x62,
+	0x6c, 0x6f, 0x63, 0x6b, 0x5f, 0x68, 0x65, 0x69, 0x67, 0x68, 0x74, 0x18, 0x03, 0x20, 0x01, 0x28,
+	0x04, 0x52, 0x0b, 0x62, 0x6c, 0x6f, 0x63, 0x6b, 0x48, 0x65, 0x69, 0x67, 0x68, 0x74, 0x32, 0x0e,
+	0x0a, 0x0c, 0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x49, 0x6e, 0x64, 0x65, 0x78, 0x65, 0x72, 0x42, 0x0a,
+	0x5a, 0x08, 0x2e, 0x2f, 0x3b, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x33,
 }
 
 var (
@@ -274,11 +345,12 @@ func file_token_indexer_proto_rawDescGZIP() []byte {
 	return file_token_indexer_proto_rawDescData
 }
 
-var file_token_indexer_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
+var file_token_indexer_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_token_indexer_proto_goTypes = []interface{}{
 	(*TokenContractDetails)(nil), // 0: ankrscan.tokenindexer.TokenContractDetails
 	(*TokenBalance)(nil),         // 1: ankrscan.tokenindexer.TokenBalance
 	(*TokenContractRequest)(nil), // 2: ankrscan.tokenindexer.TokenContractRequest
+	(*TokenBalanceRequest)(nil),  // 3: ankrscan.tokenindexer.TokenBalanceRequest
 }
 var file_token_indexer_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -330,6 +402,18 @@ func file_token_indexer_proto_init() {
 				return nil
 			}
 		}
+		file_token_indexer_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*TokenBalanceRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -337,7 +421,7 @@ func file_token_indexer_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_token_indexer_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   3,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
